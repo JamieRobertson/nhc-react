983d1fdce9e82f054e8183777ccf82f3c7a57e61
Jamie Robertson <jamie@jamierobertson.io>
2016-08-22 16:59:10 +0200

    Add more realistic data

--- data/data.js
+++ data/data.js
@@ -1,17 +1,205 @@
 let data = [
-	{
-		"name": "aaa", 
-		"description": "a Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.", 
-		"coords": [ 51.5 , -0.21 ]
-	}, {
-		"name": "bbb", 
-		"description": "b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.", 
-		"coords": [ 51.5 , -0.21 ]
-	}, {
-		"name": "ccc", 
-		"description": "c Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.", 
-		"coords": [ 51.5 , -0.21 ]
-	}
+    {
+        "name" : "4Play",
+        "type" : "soundsystem", 
+        "description" : "House, garage, R&B, hip hop, bashment, reggae",
+        "coords" : [ 51.526301, -0.214019 ]
+    }, {
+    	"name" : "Aba-shan-ti",
+    	"type" : "soundsystem", 
+        "description" : "Roots, reggae, dub",
+        "coords" : [ 51.524592, -0.210865 ]
+    }, {
+    	"name" : "Arts A Light",
+    	"type" : "soundsystem", 
+        "description" : "R&B, reggae, hip hop, classic garage",
+        "coords" : [  51.521492, -0.21051 ]
+    }, {
+    	"name" : "Blackstar Showtime",
+    	"type" : "soundsystem", 
+        "description" : "Reggae, ragga, soul",
+        "coords" : [ 51.524312, -0.209223 ]
+    }, {
+    	"name" : "Channel One",
+    	"type" : "soundsystem", 
+        "description" : "Roots & culture",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+    	"name" : "CMC Matrix",
+    	"type" : "soundsystem", 
+        "description" : "Drum & bass",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+    	"name" : "Different Strokes",
+    	"type" : "soundsystem", 
+        "description" : "Anything goes, house-party style",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+    	"name" : "Disco Hustlers",
+    	"type" : "soundsystem", 
+        "description" : "House music through the ages",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+    	"name" : "Disya Generation",
+    	"type" : "soundsystem", 
+        "description" : "R&B, hip hop, bashment, soca",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Gaz's Rockin' Blues",
+        "type" : "soundsystem", 
+        "description" : "Classic ska, revival, New Orlean's R&B",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "GI Roadshow",
+        "type" : "soundsystem", 
+        "description" : "Reggae, bashment, salsa, R&B",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Gladdy Wax's Roadshow",
+        "type" : "soundsystem", 
+        "description" : "Roots & culture, reggae & ska",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "GT Flex Roadshow",
+        "type" : "soundsystem", 
+        "description" : "R&B, ragga, soul, old school, revival",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "High Grade",
+        "type" : "soundsystem", 
+        "description" : "Reggae & roots",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Jah Observer",
+        "type" : "soundsystem", 
+        "description" : "Roots & culture",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Killawatt",
+        "type" : "soundsystem", 
+        "description" : "Reggae, ragga, dancehall",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "King Tubbys",
+        "type" : "soundsystem", 
+        "description" : "Reggae, dancehall, bashment, R&B",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Latin Rave Street Jam",
+        "type" : "soundsystem", 
+        "description" : "Latin & salsa",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Lord Gelly's",
+        "type" : "soundsystem", 
+        "description" : "Reggae, R&B, soca & beyond",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Mangrove Sound",
+        "type" : "soundsystem", 
+        "description" : "Soca & reggae",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Mastermind Roadshow",
+        "type" : "soundsystem", 
+        "description" : "Upfront R&B, hip hop, soul, old school, reggae",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Mellowtone",
+        "type" : "soundsystem", 
+        "description" : "Soul, ragga, R&B, revival",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Metro Glory",
+        "type" : "soundsystem", 
+        "description" : "House to roots",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Nasty Love Mixing Lab",
+        "type" : "soundsystem", 
+        "description" : "Reggae, ragga, hip hop, R&B",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "People's Sound",
+        "type" : "soundsystem", 
+        "description" : "Old & new reggae",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Pineapple Tribe",
+        "type" : "soundsystem", 
+        "description" : "Techno & breaks",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Rapattack",
+        "type" : "soundsystem", 
+        "description" : "Old school soul, rare groove, UKG, funky",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Rough But Sweet",
+        "type" : "soundsystem", 
+        "description" : "Reggae, dub, calypso, garage",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Sancho Panza",
+        "type" : "soundsystem", 
+        "description" : "Funky house, disco, techno",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Saxon Studio",
+        "type" : "soundsystem", 
+        "description" : "Ragga & reggae",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Sir Lloyd",
+        "type" : "soundsystem", 
+        "description" : "R&B & reggae",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Sir Valdez",
+        "type" : "soundsystem", 
+        "description" : "R&B, hip hop, ragga",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "SSP Sound System",
+        "type" : "soundsystem", 
+        "description" : "Funky & soulful house, UKG, classics",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "The Fun Bunch",
+        "type" : "soundsystem", 
+        "description" : "R&B, hip hop, garage, boogie",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Virgo International",
+        "type" : "soundsystem", 
+        "description" : "Reggae, soca, hip hop, R&B, garage",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Westbourne Park",
+        "type" : "station", 
+        "description" : "Exit only from 11:00-18:00 station closes at 23:30",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Ladbroke Grove",
+        "type" : "station", 
+        "description" : "Station closed",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Royal Oak",
+        "type" : "station", 
+        "description" : "exit only from 11:00-18:00 station closes at 18:00",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Bayswater",
+        "type" : "station", 
+        "description" : "Open both days",
+        "coords" : [ 51.51, -0.20 ]
+    }, {
+        "name" : "Notting Hill Gate",
+        "type" : "station", 
+        "description" : "12:00-19:00 exit only no circle & district line access",
+        "coords" : [ 51.51, -0.20 ]
+    }
 ];
 
 export default data;
