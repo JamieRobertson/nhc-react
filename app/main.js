465f0eff326571be22e370b311ff17c0e6ec684c
Jamie Robertson <jamie@jamierobertson.io>
2016-08-30 18:24:08 +0200

    Update data to 2016 version + add genres

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
-import data from './data/data.js';
+import data from './data/data2016.js';
 import './sass/app.sass';
 
 ReactDOM.render(<App data={data} />, document.getElementById('app'));
